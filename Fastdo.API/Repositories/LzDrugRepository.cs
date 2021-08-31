b1992e028586cb3c25b1ff6b31725c7462505a0e
m.abdelaal <m.abdelaal@wesamsoft.com>
2021-08-31 20:45:59 +0200

    DisplayLzDrugeInPharmacy

--- Fastdo.API/Repositories/LzDrugRepository.cs
+++ Fastdo.API/Repositories/LzDrugRepository.cs
@@ -55,7 +55,7 @@ namespace Fastdo.API.Repositories
         {
 
             var sourceData=GetAll()
-            .Where(d => d.PharmacyId == UserId && !d.Exchanged)
+            .Where(d => d.PharmacyId == UserId)
             .OrderBy(d=>d.Name)
             .Select(d => new LzDrugModel_BM
             {
